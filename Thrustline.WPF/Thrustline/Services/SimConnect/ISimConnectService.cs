bdc53bf14a676c565764eac686f12610f70ee030
AndyD9 <andy.dodakal@gmail.>
2026-07-21 19:42:37 +0200

    chore: remove stale legacy files after rework merge

--- Thrustline.WPF/Thrustline/Services/SimConnect/ISimConnectService.cs
+++
@@ -1,9 +0,0 @@
-namespace Thrustline.Services.SimConnect;
-
-public interface ISimConnectService
-{
-    event Action<SimData>? SimDataReceived;
-    string Status { get; } // "connected" | "mock" | "disconnected"
-    void Start();
-    void Stop();
-}
